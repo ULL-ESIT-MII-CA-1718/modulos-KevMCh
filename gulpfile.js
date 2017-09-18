0a11fb8948848159ca9e5f3839dd16d19e08ee54
Kevin <marchinkev@gmail.com>
2017-09-18 19:59:52 +0100

    Debugg Task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var shell = require('gulp-shell');
 
 gulp.task("default", ["test"]);
 
-gulp.task('run', shell.task('node ./src/main.js'));
+gulp.task("run", shell.task('node ./src/main.js'));
+
+/*
+En versiones v8.*  chrome://inspect en el navegador
+https://nodejs.org/en/docs/inspector/
+*/
+gulp.task('debugger', shell.task('node --inspect-brk src/main.js'));
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 var input = require("./input.json");
 var Table = require("./Table.js").Table;
 
+debugger;
+
 var table = new Table();
 console.log(table.drawAllTable(input));
